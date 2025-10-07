197c885bab280a8254565d1e97e636ab20f27ec9
Leah224 <Leahc9799@gmail.com>
2025-10-07 09:17:37 -0400

    Finished post.mjs

---
+++ Post.mjs
@@ -0,0 +1,12 @@
+// Post.mjs
+export default class Post {
+  constructor(title, content) {
+    this.title = title;
+    this.content = content;
+  }
+
+  publish() {
+    console.log(`📢 ${this.title}`);
+    console.log(this.content);
+  }
+}
