3b31f68a6f19f2cd7b3d76f878016d8ee307ada7
Leah224 <Leahc9799@gmail.com>
2025-10-07 09:16:01 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+// app.js
+async function loadConfig() {
+  const { setLightTheme, setDarkTheme } = await import("./theme.mjs");
+  const hour = new Date().getHours();
+  if (hour < 18) {
+    setLightTheme();
+  } else {
+    setDarkTheme();
+  }
+}
+loadConfig();
