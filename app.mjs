1e0decfaffb1666e2de63d2b3183735093eccb0c
Leah224 <Leahc9799@gmail.com>
2025-10-07 09:16:47 -0400

    Finished app.mjs

---
+++ app.mjs
@@ -0,0 +1,8 @@
+// app.mjs
+import "./globalConfig.mjs";
+import { capitalize, reverse, add, multiply } from "./utils/index.mjs";
+
+console.log(capitalize("gryffindor"));
+console.log(reverse("slytherin"));
+console.log(add(7, 3));
+console.log(multiply(2, 6));
